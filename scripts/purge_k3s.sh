9dfa1f73315e7bfeb26243f8e6e342f9ed5d5f1e
Lucas Athayde <athaydaooo@gmail.com>
2025-02-21 09:49:55 -0300

    fix config

--- scripts/purge_k3s.sh
+++ scripts/purge_k3s.sh
@@ -14,19 +14,7 @@ rm -rf /var/lib/cni
 rm -rf /etc/cni
 rm -rf /var/run/flannel
 
-# Clean up network interfaces
-ip link delete cni0
-ip link delete flannel.1
-
 # Remove k3s binary
 rm -f /usr/local/bin/k3s
-
-# Clean iptables
-iptables -F
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
 
 echo "K3s has been completely purged from the system"
